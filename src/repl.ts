36f9a3dbe267245a6172051d7437eeb20814783c
faelim <jgrand-gh@users.noreply.github.com>
2025-08-07 00:32:46 -0500

    added map and mapback functions, as well as PokeAPI calls

--- src/repl.ts
+++ src/repl.ts
@@ -1,9 +1,9 @@
 import { State } from "./state.js";
 
-export function startREPL(state: State): void {
+export async function startREPL(state: State): Promise<void> {
     state.readline.prompt();
 
-    state.readline.on('line', (input) => {
+    state.readline.on('line', async (input) => {
         const cleanedInput = cleanInput(input);
         if (cleanedInput.length === 0) {
             state.readline.prompt();
@@ -12,11 +12,17 @@ export function startREPL(state: State): void {
 
         const command = state.commands[cleanedInput[0]];
         if (!command) {
-            console.log("Unknown command");
-        } else {       
-            command.callback(state);
+            console.log('Unknown command. Type "help" for a list of commands.');
+            state.readline.prompt();
+            return;
+        } else {
+            try {
+                await command.callback(state);
+            } catch (e) {
+                console.error(`command failed due to: ${(e as Error).message}`);
+            }
+            state.readline.prompt();
         }
-        state.readline.prompt();
     });
 }
 
