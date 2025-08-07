f32c980c9f3b54bb740866e85e9102ab6b470af3
faelim <jgrand-gh@users.noreply.github.com>
2025-08-07 03:36:55 -0500

    added caching and respective unit test

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { startREPL } from "./repl.js";
 import { initState } from "./state.js";
 
 async function main() {
-  const state = initState();
-  await startREPL(state);
+    const state = initState(5 * 60 * 1000);
+    await startREPL(state);
 }
 
 main();
